6e9430509cd3eaed2db1993d3d3644d20311e8b5
Charlie Barth <charliepbarth@gmail.com>
2020-07-12 16:54:05 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,26 @@
+class Post
+  attr_accessor :author, :title, :genre
+ 
+  @@all = []
+ 
+  def initialize(title)
+    @title = title
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+ 
+  def self.all
+    @@all
+  end
+    
+  def author_name
+    if self.author.name == self.author.name
+      self.author.name
+    else 
+      nil
+    end
+  end
+end
